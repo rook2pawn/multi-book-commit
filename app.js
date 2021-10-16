eddd4b9bcea3e6ac3bb64a820935a78b480bd022
rook2pawn <rook2pawn@gmail.com>
2021-10-16 01:41:09 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const choo = require("choo");
+const html = require("nanohtml");
+const devtools = require("choo-devtools");
+
+const css = require("sheetify");
+css("./app.css");
+
+module.exports = () => {
+  const app = choo();
+
+  app.use(devtools());
+  app.use((state) => {
+    state.logger = false;
+  });
+  function mainView(state, emit) {
+    return html`<body>
+      <div></div>
+    </body>`;
+  }
+  app.route("/", mainView);
+  app.use((state, emitter) => {});
+  app.mount("body");
+};
