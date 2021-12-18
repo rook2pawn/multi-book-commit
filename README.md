7fd06daf68ba31e897eca4089717d2700037f9f6
rook2pawn <rook2pawn@gmail.com>
2021-12-18 13:35:22 -0800

    update

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ Run the following in separate terminals
     npm run start-backend
     npm run watch
 ```
+
+Point your browser to http://localhost:8080 (Proxy)
--- app/app.js
+++ app/app.js
@@ -14,10 +14,28 @@ module.exports = () => {
   });
   function mainView(state, emit) {
     return html`<body>
-      <div><h4>Choo App Starter</h4></div>
+      <div>
+        <h4>Choo App Starter</h4>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </div>
+    </body>`;
+  }
+  function aboutView(state, emit) {
+    return html`<body>
+      <div>
+        <h4>About Page</h4>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </div>
     </body>`;
   }
   app.route("/", mainView);
+  app.route("/about", aboutView);
   app.use((state, emitter) => {});
   app.mount("body");
 };
